b933e1068fa954a97683ec85508251b3fef5e61b
Toto Corvidoni <totocorvidoni@gmail.com>
2019-02-13 09:40:31 -0300

    Add simple form validation and refactor.

--- books.js
+++ books.js
@@ -1,31 +1,24 @@
 /* Set Up */
 
-booksTable = document.querySelector('tbody');
-formToggle = document.querySelector('#toggle-form');
-bookForm   = document.querySelector('#book-form');
-
-formToggle.addEventListener('click', () => bookForm.classList.toggle('hidden'));
-bookForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  submitBook();
-});
-
-myLibrary = [];
-console.table(myLibrary);
+const booksTable = document.querySelector('tbody');
+const formToggle = document.querySelector('#toggle-form');
+const bookForm = document.querySelector('#book-form');
+const myLibrary = [];
 
 /* Logic */
 
 class Book {
-
   constructor(title, author, pages, read) {
-    this.title  = title;
+    this.title = title;
     this.author = author;
-    this.pages  = pages;
-    this.read   = read
+    this.pages = pages;
+    this.read = read;
   }
 
   info() {
-    status = (this.read) ? "You have read this book.": "You haven't read this book yet."
+    const status = this.read
+      ? 'You have read this book.'
+      : "You haven't read this book yet.";
     return `${this.title} by ${this.author}. Pages: ${this.pages}. ${status}`;
   }
 
@@ -34,114 +27,101 @@ class Book {
   }
 }
 
-/*function Book(title, author, pages, read) {
-  this.title  = title,
-  this.author = author,
-  this.pages  = pages,
-  this.read   = read
-}
-
-Book.prototype.info = function() {
-  status = (this.read) ? "You have read this book.": "You haven't read this book yet."
-  return `${this.title} by ${this.author}. Pages: ${this.pages}. ${status}`;
-}
-
-Book.prototype.readToggle = function() {
-  this.read = !this.read;
-}*/
-function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read))
-  saveLibrary();
-  render();
-}
-
-function submitBook() {
-  addBookToLibrary(bookForm.title.value,
-                   bookForm.author.value,
-                   bookForm.pages.value,
-                   bookForm.read.value);
-  bookForm.reset();
-}
-
-function removeBook() {
-  index = this.parentElement.dataset.bookId;
-  myLibrary.splice(index, 1);
-  saveLibrary();
-  render();
-}
-
-function readBook() {
-  index = this.parentElement.dataset.bookId;
-  book = myLibrary[index];
-  book.readToggle();
-  saveLibrary();
-  render();
-}
-
 function saveLibrary() {
   localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
 // Parses from local storage and makes build new Book objects.
 function loadLibrary() {
-  books = JSON.parse(localStorage.getItem('myLibrary'));
-  books.forEach(function(book) {
+  const books = JSON.parse(localStorage.getItem('myLibrary'));
+  books.forEach(book => {
     myLibrary.push(new Book(...Object.values(book)));
-  }) 
-}
-
-/* Layout */
-
-function render() {
-  clearTable();
-  myLibrary.forEach(function(book) {
-    row = createBookRow(book);
-    booksTable.appendChild(row);
   });
 }
 
-function createBookRow(book) {
-  row = document.createElement('tr');
-  row.classList.add('book');
-  row.setAttribute('data-book-id', myLibrary.indexOf(book));
-  for(let prop in book) {
-    if (book.hasOwnProperty(prop)) {
-      row.appendChild(createBookCell(book[prop]));  
-    }
-  }
-  row.appendChild(createRemoveButton());
-  row.appendChild(createReadButton());
-  return row;
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
+  saveLibrary();
+  render();
 }
 
-function createBookCell(content) {
-  cell = document.createElement('td');
-  content = document.createTextNode(content);
-  cell.appendChild(content);
-  return cell;
+function submitBook() {
+  addBookToLibrary(
+    bookForm.title.value,
+    bookForm.author.value,
+    bookForm.pages.value,
+    bookForm.read.value,
+  );
+  bookForm.reset();
 }
+
+function removeBook() {
+  const index = this.parentElement.dataset.bookId;
+  myLibrary.splice(index, 1);
+  saveLibrary();
+  render();
+}
+
+function readBook() {
+  const index = this.parentElement.dataset.bookId;
+  const book = myLibrary[index];
+  book.readToggle();
+  saveLibrary();
+  render();
+}
+
+/* Layout */
 
 function clearTable() {
   booksTable.innerHTML = '';
 }
 
+function createBookCell(content) {
+  const cell = document.createElement('td');
+  const contentNode = document.createTextNode(content);
+  cell.appendChild(contentNode);
+  return cell;
+}
+
 function createRemoveButton() {
-  button = document.createElement('button');
-  text = document.createTextNode('remove');
-  button.classList.add('remove');
+  const button = document.createElement('button');
+  const text = document.createTextNode('X');
+  button.classList.add('X');
   button.addEventListener('click', removeBook);
   button.appendChild(text);
-  return button; 
+  return button;
 }
 
 function createReadButton() {
-  button = document.createElement('button');
-  text = document.createTextNode('read');
-  button.classList.add('read');
+  const button = document.createElement('button');
+  const text = document.createTextNode('✓');
+  button.classList.add('✓');
   button.addEventListener('click', readBook);
   button.appendChild(text);
   return button;
 }
+
+function createBookRow(book) {
+  const row = document.createElement('tr');
+  row.classList.add('book');
+  row.setAttribute('data-book-id', myLibrary.indexOf(book));
+  Object.values(book).forEach(property =>
+    row.appendChild(createBookCell(property)),
+  );
+  row.appendChild(createRemoveButton());
+  row.appendChild(createReadButton());
+  return row;
+}
+
+function render() {
+  clearTable();
+  myLibrary.forEach(book => {
+    const row = createBookRow(book);
+    booksTable.appendChild(row);
+  });
+}
+
+/* Form Validation */
 
 /*  */
 
@@ -149,7 +129,14 @@ if (localStorage.getItem('myLibrary')) {
   loadLibrary();
 } else {
   addBookToLibrary('Le Fantastique Totoret', 'Totoret', 5, true);
-  addBookToLibrary('Voyage avec Pupe LéPu', 'Pupe',  784, false);
-  addBookToLibrary('la conjura de los necios', 'toole',  500, true);
+  addBookToLibrary('Voyage avec Pupe LéPu', 'Pupe', 784, false);
+  addBookToLibrary('la conjura de los necios', 'toole', 500, true);
 }
-render()
+
+render();
+
+formToggle.addEventListener('click', () => bookForm.classList.toggle('hidden'));
+bookForm.addEventListener('submit', event => {
+  event.preventDefault();
+  if (bookForm.reportValidity()) submitBook();
+});
